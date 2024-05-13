4a0e0d3de9e410543723b273443c93130a5a97cb
Bjohan23 <bventurajohanjh@uss.edu.pe>
2024-05-13 11:49:43 -0500

    Actualizar dependencia "env" a la versión 0.0.2

--- api/crear_tablas.php
+++ api/crear_tablas.php
@@ -2,28 +2,31 @@
 include_once "encabezado.php";
 include_once "funciones.php";
 
-$host = "localhost";
+$host = "monorail.proxy.rlwy.net";
 $usuario = "root";
-$password = "";
+$password = "OEUAbePCiTVtqIZGhCArxNlbTeGZXWzM";
 $resultados = [];
 
 $crear_bd = "CREATE DATABASE IF NOT EXISTS botanero_ventas";
 
 
 $sentencias = [
-	["tabla"=>"categorias",
-	"sentencia" => 
-		'CREATE TABLE categorias(
+    [
+        "tabla" => "categorias",
+        "sentencia" =>
+        'CREATE TABLE categorias(
             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             tipo ENUM("PLATILLO", "BEBIDA") NOT NULL,
             nombre VARCHAR(50) NOT NULL,
             descripcion VARCHAR(255)
-	);'],
+	);'
+    ],
 
 
-	["tabla"=>"insumos",
-		"sentencia" =>
-		'CREATE TABLE insumos(
+    [
+        "tabla" => "insumos",
+        "sentencia" =>
+        'CREATE TABLE insumos(
             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             codigo VARCHAR(100) NOT NULL,
             nombre VARCHAR(100) NOT NULL,
@@ -31,29 +34,35 @@ $sentencias = [
             precio DECIMAL(6,2) NOT NULL,
             tipo ENUM("PLATILLO", "BEBIDA") NOT NULL,
             categoria BIGINT UNSIGNED NOT NULL
-		);'],
+		);'
+    ],
 
-	["tabla"=>"informacion_negocio",
-		"sentencia" => 'CREATE TABLE informacion_negocio(
+    [
+        "tabla" => "informacion_negocio",
+        "sentencia" => 'CREATE TABLE informacion_negocio(
             nombre VARCHAR(100),
             telefono VARCHAR(15),
             numeroMesas TINYINT,
             logo VARCHAR(255)
-		);'],
+		);'
+    ],
 
-	["tabla"=>"usuarios",
-		"sentencia" =>
-		'CREATE TABLE usuarios(
+    [
+        "tabla" => "usuarios",
+        "sentencia" =>
+        'CREATE TABLE usuarios(
             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             correo VARCHAR(100) NOT NULL,
             nombre VARCHAR(100) NOT NULL,
             telefono VARCHAR(20) NOT NULL,
             password VARCHAR(255) NOT NULL
-		);'],
+		);'
+    ],
 
-	["tabla"=>"ventas",
-		"sentencia" =>
-		'CREATE TABLE ventas(
+    [
+        "tabla" => "ventas",
+        "sentencia" =>
+        'CREATE TABLE ventas(
             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             idMesa TINYINT NOT NULL,
             cliente VARCHAR(100),
@@ -61,23 +70,26 @@ $sentencias = [
             total DECIMAL(6,2) NOT NULL,
             pagado DECIMAL(6,2) NOT NULL,
             idUsuario BIGINT UNSIGNED NOT NULL
-		);'],
+		);'
+    ],
 
-	["tabla"=>"insumos_venta",
-		"sentencia" =>
-		'CREATE TABLE insumos_venta(
+    [
+        "tabla" => "insumos_venta",
+        "sentencia" =>
+        'CREATE TABLE insumos_venta(
             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             idInsumo BIGINT UNSIGNED NOT NULL,
             precio DECIMAL(6,2) NOT NULL,
             cantidad INT NOT NULL,
             idVenta BIGINT UNSIGNED
-		);']
+		);'
+    ]
 ];
 
 $conexion = new PDO("mysql:host=$host", $usuario, $password);
 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $creada_bd = $conexion->exec($crear_bd);
-if($creada_bd) array_push($resultados, "Base de datos creada correctamente");
+if ($creada_bd) array_push($resultados, "Base de datos creada correctamente");
 $conexion->exec('USE botanero_ventas');
 #Ahora crear las tablas
 //$conexion->close();
@@ -86,12 +98,10 @@ $conexion = new PDO("mysql:host=$host;dbname=botanero_ventas", $usuario, $passwo
 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 foreach ($sentencias as $sentencia) {
-	$conexion->exec($sentencia["sentencia"]);
+    $conexion->exec($sentencia["sentencia"]);
 
-	array_push($resultados, "Tabla {$sentencia["tabla"]} creada correctamente");
-
+    array_push($resultados, "Tabla {$sentencia["tabla"]} creada correctamente");
 }
 
 $conexion = null;
 echo json_encode($resultados);
-
